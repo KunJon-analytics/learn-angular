c84dd7fe66f8cd8ffa25ba2847ed3ad19da86b83
KunJon-analytics <johnadelakun1@gmail.com>
2023-06-19 18:44:43 +0100

    fixed favorite service error

--- src/app/products/favorites.service.ts
+++ src/app/products/favorites.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Product } from './product';
 import { ProductsService } from './products.service';
 import { Observable, filter, mergeMap, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FavoritesService extends ProductsService {
-  constructor() {
-    super();
+  constructor(http: HttpClient) {
+    super(http);
   }
   override getProducts(): Observable<Product[]> {
     return super.getProducts().pipe(
